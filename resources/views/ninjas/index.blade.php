94d2654f3f52b9b0db122910608bd799e24827ec
naraabinnadhikari <narayanabin223@gmail.com>
2024-10-30 16:35:22 +0545

    initial setup

---
+++ resources/views/ninjas/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ninja Network</title>
+</head>
+<body>
+    <h2> Currently abailable NINJAS</h2>
+
+    <ul>
+        <li>Ninjas Here</li>
+    </ul>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MY NETWORK</title>
+</head>
+<body>
+    <h1>Welcome to the Laravel Network.</h1>
+    <p>Click the button below to view the list of Clients.</p>
+
+    <a href="/ninjas" class="btn">Find Your Clients.</a>
+</body>
+</html>
